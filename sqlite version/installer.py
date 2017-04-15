25102bb98c47353560e1018d91aea74e6147edbe
Shivam Tayal <tayal.2shivam@gmail.com>
2017-04-15 20:21:40 +0530

    Update installer.py

--- sqlite version/installer.py
+++ sqlite version/installer.py
@@ -1,6 +1,6 @@
 import os
 def run_command(module):
-    os.system("C:/Python27/python.exe -m pip install --proxy=https://cse150001035:j6ym4hh3@webproxy.indore.iiti.ac.in:8080 "+module)
+    os.system("C:/Python27/python.exe -m pip install "+module)
 def printerror(module):
     print("ERROR : Could not download module "+module)
 modules = ["requests","pdfminer","lxml","urllib3","PyPDF2","xlsxwriter"]
