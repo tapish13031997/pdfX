fb0105346020b4495517c47da7afd8c960bde479
cxor <cse150001035@iiti.ac.in>
2017-04-15 16:26:45 +0530

    added proxy to installer

--- sqlite version/installer.py
+++ sqlite version/installer.py
@@ -1,9 +1,9 @@
 import os
 def run_command(module):
-    os.system("C:/Python27/python.exe -m pip install "+module)
+    os.system("C:/Python27/python.exe -m pip install --proxy=https://cse150001035:j6ym4hh3@webproxy.indore.iiti.ac.in:8080 "+module)
 def printerror(module):
     print("ERROR : Could not download module "+module)
-modules = ["requests","pdfminer","lxml","urllib3","PyPDF2"]
+modules = ["requests","pdfminer","lxml","urllib3","PyPDF2","xlsxwriter"]
 for module in modules:
     try:
         run_command(module)
